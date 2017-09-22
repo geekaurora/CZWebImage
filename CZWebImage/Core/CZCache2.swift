fb445f018bf325431aad8bc7c6fccdbd532c7d35
Cheng Zhang <showt2me@gmail.com>
2017-09-22 11:45:45 -0700

    adding CZCache2

---
+++ CZWebImage/Core/CZCache2.swift
@@ -0,0 +1,63 @@
+//
+//  CZCache2.swift
+//  CZWebImage
+//
+//  Created by Cheng Zhang on 9/22/17.
+//  Copyright © 2017 Cheng Zhang. All rights reserved.
+//
+
+import UIKit
+
+@objc class CZCache2: NSObject {
+    static let sharedInsance = CZCache2()
+    
+    fileprivate var ioQueue: DispatchQueue
+    
+    fileprivate var cachedItemsInfoLock: CZMutexLock<[String: Any]>
+    fileprivate var hasCachedItemsInfoToFlushToDisk: Bool = false
+    fileprivate var memCache: NSCache<AnyObject, AnyObject>
+    fileprivate var fileManager: FileManager
+    fileprivate static let cacheFolder: String = {
+        let cacheFolder = CZWebImageUtils.documentFolder() + "CZCache"
+        let fileManager = FileManager()
+        if !fileManager.fileExists(atPath: cacheFolder) {
+            do {
+                try fileManager.createDirectory(atPath: cacheFolder, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                assertionFailure("Failure of creating folder! Error - \(error.localizedDescription); Folder - \(cacheFolder)")
+            }
+        }
+        return cacheFolder
+    }()
+    fileprivate(set) var maxCacheAge: UInt
+    fileprivate(set) var maxCacheSize: UInt
+    
+    init(maxCacheAge: UInt = 0, maxCacheSize: UInt = 0) {
+        ioQueue = DispatchQueue(label: "com.tony.cache.ioQueue",
+                                qos: .userInitiated,
+                                attributes: .concurrent)
+        
+        fileManager = FileManager()
+        
+        // Memory cache
+        memCache = NSCache()
+        memCache.countLimit = 1000
+        memCache.totalCostLimit = 1000 * 1024 * 1024
+        
+        cachedItemsInfoLock = CZMutexLock([:])
+//        _cachedItemsInfo = [self loadCachedItemsInfoFromDisk] ? : [NSMutableDictionary new];
+//        [self cleanDiskWithCompletionBlock:nil];
+
+        self.maxCacheAge = maxCacheAge
+        self.maxCacheSize = maxCacheSize
+        super.init()
+    }
+    
+    func cacheFilePath(for urlStr: String) -> String {
+        return ""
+    }
+}
+
+fileprivate extension CZCache2 {
+    
+}
