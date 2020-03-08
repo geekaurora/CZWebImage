10799a7c8c5d0e48941ed4500f3a5a74e97c85b2
Cheng Zhang <showt2me@gmail.com>
2020-03-08 21:56:10 +0900

    [iOS] Update minimum support iOS to 11.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CZWebImage",
     platforms: [
-        .iOS(.v9),
+        .iOS(.v11),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.0.0"),
-         .package(url: "https://github.com/geekaurora/CZNetworking.git", from: "3.0.0")
+         .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.0.1"),
+         .package(url: "https://github.com/geekaurora/CZNetworking.git", from: "3.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
