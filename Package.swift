0f9c9a5730961c1b92b67509abdb19122f76dd5b
Cheng Zhang <showt2me@gmail.com>
2020-04-10 17:38:02 +0900

    Update CZNetworking

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/geekaurora/CZNetworking.git", from: "3.0.5")
+         .package(url: "https://github.com/geekaurora/CZNetworking.git", from: "3.0.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
