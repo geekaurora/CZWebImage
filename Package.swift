01e396c1950f1b4362dab39adf070867f4e6d367
Cheng Zhang <showt2me@gmail.com>
2021-03-21 18:16:43 +0900

    Before import CZWebFileDownloader

--- Package.swift
+++ Package.swift
@@ -4,28 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "CZWebImage",
-    platforms: [
-        .iOS(.v11),
-    ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "CZWebImage",
-            type: .dynamic,
-            targets: ["CZWebImage"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.2.7"),
-        .package(url: "https://github.com/geekaurora/CZNetworking.git", from: "3.2.2"),
-        .package(url: "https://github.com/geekaurora/CZHttpFileCache.git", from: "2.2.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "CZWebImage",
-            dependencies: ["CZUtils", "CZNetworking", "CZHttpFileCache"]),
-    ]
+  name: "CZWebImage",
+  platforms: [
+    .iOS(.v11),
+  ],
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(
+      name: "CZWebImage",
+      type: .dynamic,
+      targets: ["CZWebImage"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/geekaurora/CZUtils.git", from: "3.2.7"),
+    .package(url: "https://github.com/geekaurora/CZNetworking.git", from: "3.2.2"),
+    .package(url: "https://github.com/geekaurora/CZHttpFileCache.git", from: "2.2.0"),
+    .package(url: "https://github.com/geekaurora/CZWebFileDownloader.git", from: "1.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "CZWebImage",
+      dependencies: ["CZUtils", "CZNetworking", "CZHttpFileCache", "CZWebFileDownloader"]),
+  ]
 )
