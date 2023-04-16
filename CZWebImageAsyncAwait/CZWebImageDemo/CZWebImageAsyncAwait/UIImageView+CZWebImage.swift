379dbaff8773827e0f2dd0ea94b9c76a2c605c69
Cheng Zhang <showt2me@gmail.com>
2023-04-16 16:34:04 -0700

    works

--- CZWebImageAsyncAwait/CZWebImageDemo/CZWebImageAsyncAwait/UIImageView+CZWebImage.swift
+++ CZWebImageAsyncAwait/CZWebImageDemo/CZWebImageAsyncAwait/UIImageView+CZWebImage.swift
@@ -24,33 +24,10 @@ extension UIImageView {
     guard let url = url else { fatalError() }
     
     Task {
-      let image = try! await URLSessionManager.shared.fetch(url:)
-      // self.image = image
-      self.layoutIfNeeded()           
+      let image = try await URLSessionManager.shared.fetch(url:url)
+      self.image = image
+      self.layoutIfNeeded()
     }
-    
-    //        let priority: Operation.QueuePriority = options.contains(.highPriority) ? .veryHigh : .normal
-    //        CZWebImageManager.shared.downloadImage(with: url, cropSize: cropSize, priority: priority) { [weak self] (image, error, fromCache) in
-    //            guard let `self` = self, self.czImageUrl == url else { return }
-    //
-    //            if let error = error {
-    //                let isCancelled = (error.retrievedCode == NSURLErrorCancelled)
-    //                if !isCancelled {
-    //                    assertionFailure("Failed to download image: \(url). Error - \(error.localizedDescription)")
-    //                }
-    //                return
-    //            }
-    //
-    //
-    //            MainQueueScheduler.sync {
-    //                if !fromCache && options.contains(.fadeInAnimation) {
-    //                    self.fadeIn()
-    //                }
-    //                self.image = image
-    //                self.layoutIfNeeded()
-    //                completion?(image, error)
-    //            }
-    //        }
   }
   
 }
