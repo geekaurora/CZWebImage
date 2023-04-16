bc88963fdf08aab06f221deaaa48c16cd22ef4c9
Cheng Zhang <showt2me@gmail.com>
2023-04-16 16:35:47 -0700

    Minor update.

--- CZWebImageAsyncAwait/CZWebImageDemo/CZWebImageAsyncAwait/URLSessionManager.swift
+++ CZWebImageAsyncAwait/CZWebImageDemo/CZWebImageAsyncAwait/URLSessionManager.swift
@@ -7,6 +7,9 @@ actor URLSessionManager {
   private var memoryCache = [URL: UIImage]()
   
   public func fetch(url: URL) async throws -> UIImage? {
+    if let cachedImage = memoryCache[url] {
+      return cachedImage
+    }    
     let (data, response) = try await URLSession.shared.data(from: url)
     let image = UIImage(data: data)
     
