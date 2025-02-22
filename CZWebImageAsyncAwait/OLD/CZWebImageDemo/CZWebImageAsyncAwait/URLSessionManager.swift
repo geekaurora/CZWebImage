942a31974c9e719b60105fe1ea5c811a2ed07750
cheng zhang <showt2me@gmail.com>
2025-02-21 17:53:49 -0800

    Clean up folders

---
+++ CZWebImageAsyncAwait/OLD/CZWebImageDemo/CZWebImageAsyncAwait/URLSessionManager.swift
@@ -0,0 +1,20 @@
+import UIKit
+import CZUtils
+
+actor URLSessionManager {
+  static let shared = URLSessionManager()
+  private var memoryCache = [URL: UIImage]()
+  
+  public func fetch(url: URL) async throws -> UIImage? {
+    if let cachedImage = memoryCache[url] {
+      return cachedImage
+    }
+    // Fetch from network.
+    let (data, response) = try await URLSession.shared.data(from: url)
+    let image = UIImage(data: data)
+    
+    // Cache the image.
+    memoryCache[url] = image
+    return image
+  }
+}
