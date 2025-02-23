3fd6ed44e86ebb9bf8edec3149e4c00f70e1b639
cheng zhang <showt2me@gmail.com>
2025-02-22 18:34:18 -0800

    GroupFetchExampleView

--- CZWebImageAsyncAwait/WebImageAsyncAwaitDemo/WebImageAsyncAwaitDemo/ContentView.swift
+++ CZWebImageAsyncAwait/WebImageAsyncAwaitDemo/WebImageAsyncAwaitDemo/ContentView.swift
@@ -1,17 +1,26 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State 
-  var isSingleFetchExampleViewActive = false
+  @State var isSingleFetchExampleViewActive = false
+  @State var isGroupFetchExampleViewActive = false
 
   public var body: some View {
     if #available(iOS 16, *)  {
       return NavigationStack {
-        NavigationLink(
-          destination: SingleFetchExampleView(),
-          isActive: $isSingleFetchExampleViewActive
-        ) {
-          Text("SingleFetchExampleView")
+        VStack(alignment: .center, spacing: 20) {
+          NavigationLink(
+            destination: SingleFetchExampleView(),
+            isActive: $isSingleFetchExampleViewActive
+          ) {
+            Text("SingleFetchExampleView")
+          }
+
+          NavigationLink(
+            destination: GroupFetchExampleView(),
+            isActive: $isGroupFetchExampleViewActive
+          ) {
+            Text("GroupFetchExampleView")
+          }
         }
       }
     }
