a7ce9b14563ca9fe5e208e2c8dd3833497b616b7
cheng zhang <showt2me@gmail.com>
2025-02-22 18:11:06 -0800

    WORKS

--- CZWebImageAsyncAwait/WebImageAsyncAwaitDemo/WebImageAsyncAwaitDemo/ContentView.swift
+++ CZWebImageAsyncAwait/WebImageAsyncAwaitDemo/WebImageAsyncAwaitDemo/ContentView.swift
@@ -1,18 +1,30 @@
 import SwiftUI
 import CZWebImageAsyncAwait
 
-struct ContentView: View {
-  var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
-    }
-    .padding()
-  }
+struct Feed: Identifiable {
+  let id: UUID = UUID()
+  let imageUrl: String
+
+  static let list: [Feed] = [
+    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/01bff78eae0870a01ed491ef86405bdf.jpg"),
+    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/14729eb660b3a409368f820a053ac319.jpg"),
+    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/16c9316d8f5dbccf394f20361c96a541.jpg"),
+    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/297ee57338cb757d5bf359f5f0dd666f.jpg"),
+    .init(imageUrl: "https://d37t5b145o82az.cloudfront.net/pictures/3a7635518ee11c02c113c6cb88f1613e.jpg")
+  ]
 }
 
-#Preview {
-  ContentView()
+struct ContentView : View {
+
+  var body: some View {
+    List {
+      ForEach(Feed.list) { feed in
+        SwiftImage(feed.imageUrl) { imageView in
+          imageView
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+        }
+      }
+    }
+  }
 }
