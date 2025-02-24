e1aa178fd1b6e95d6c1e006d52dc61a4c5b74006
cheng zhang <showt2me@gmail.com>
2025-02-24 12:13:18 -0800

    built

--- CZWebImageAsyncAwait/WebImageAsyncAwaitDemo/WebImageAsyncAwaitDemo/GroupFetchExampleAsyncStreamViewState.swift
+++ CZWebImageAsyncAwait/WebImageAsyncAwaitDemo/WebImageAsyncAwaitDemo/GroupFetchExampleAsyncStreamViewState.swift
@@ -6,6 +6,28 @@ class GroupFetchExampleAsyncStreamViewState : ObservableObject {
 
   @MainActor
   func fetchImages() async {
+
+    let quakes: AsyncStream<Feed> = AsyncStream { continuation in
+
+//      for feed in FeedMock.feeds {
+//        let image = try? await SwiftConcurrentFetcher.shared.fetch(url: URL(string: feed.imageUrl)!)
+//        var newFeed = feed.mutableCopy()
+//        newFeed.image = image
+//
+//        continuation.yield(newFeed)
+//      }
+
+
+      //        let monitor = QuakeMonitor()
+      //        monitor.quakeHandler = { quake in
+      //          continuation.yield(quake)
+      //        }
+      //        continuation.onTermination = { @Sendable _ in
+      //          monitor.stopMonitoring()
+      //        }
+      //        monitor.startMonitoring()
+    }
+
     self.feeds = try? await withThrowingTaskGroup(of: Feed.self) { group in
       for feed in FeedMock.feeds {
         group.addTask {
