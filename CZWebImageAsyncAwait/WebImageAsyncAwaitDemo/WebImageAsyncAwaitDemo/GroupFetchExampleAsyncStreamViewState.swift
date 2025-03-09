c56187a18a83e4093696bebdd35f690f0dfc23a1
Tony Zhang <756876+geekaurora@users.noreply.github.com>
2025-03-08 19:05:10 -0800

    Update WebImageAsyncAwaitDemo with AsyncStream. (#15)
    * code
    
    * built
    
    * [iOS Version] Bump from v12 to v13.

---
+++ CZWebImageAsyncAwait/WebImageAsyncAwaitDemo/WebImageAsyncAwaitDemo/GroupFetchExampleAsyncStreamViewState.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+import CZWebImageAsyncAwait
+
+class GroupFetchExampleAsyncStreamViewState : ObservableObject {
+  @Published var feeds: [Feed]? = []
+
+  @MainActor
+  func fetchImages() async {
+
+    let quakes: AsyncStream<Feed> = AsyncStream { continuation in
+
+      for feed in FeedMock.feeds {
+        // let image = try? await SwiftConcurrentFetcher.shared.fetch(url: URL(string: feed.imageUrl)!)
+        //        var newFeed = feed.mutableCopy()
+        //        newFeed.image = image
+
+        // continuation.yield(newFeed)
+      }
+
+
+      //        let monitor = QuakeMonitor()
+      //        monitor.quakeHandler = { quake in
+      //          continuation.yield(quake)
+      //        }
+      //        continuation.onTermination = { @Sendable _ in
+      //          monitor.stopMonitoring()
+      //        }
+      //        monitor.startMonitoring()
+    }
+
+    self.feeds = try? await withThrowingTaskGroup(of: Feed.self) { group in
+      for feed in FeedMock.feeds {
+        group.addTask {
+          let image = try? await SwiftConcurrentFetcher.shared.fetch(url: URL(string: feed.imageUrl)!)
+          var newFeed = feed.mutableCopy()
+          newFeed.image = image
+          return newFeed
+        }
+      }
+
+      var feeds = [Feed]()
+      for try await feed in group {
+        print("Completed fetching! Feed.id = \(feed.id)")
+
+        feeds.append(feed)
+      }
+      return feeds
+    }
+  }
+
+}
